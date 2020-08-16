fb9cee61ae00caeea3cc755c271384d250de4d1f
Smruthi Srinivasan <mail2smruthi@gmail.com>
2020-08-16 17:11:33 -0400

    Some changes5

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
 
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__name__))
